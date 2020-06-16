cb5c14cd52e90aa32900ac67a42f3fd789bceed5
Jasdeep71 <jasdeepsingh0109@gmail.com>
2020-06-16 21:52:49 +0530

    updates

--- app/src/main/java/com/example/day2/Signin.java
+++ app/src/main/java/com/example/day2/Signin.java
@@ -6,23 +6,38 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Signin extends AppCompatActivity {
     private Button timeto;
+    EditText emailInput1;
+    EditText passwordInput1;
+    String email;
+    String password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signin);
-//        timeto = findViewById(R.id.button4);
-//        timeto.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                timetostart();
-//            }
-//        });
+        timeto = findViewById(R.id.button4);
+        emailInput1 = findViewById(R.id.editText);
+        passwordInput1 = findViewById(R.id.editText2);
+        timeto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                email = emailInput1.getText().toString();
+                password = passwordInput1.getText().toString();
+                if(email.isEmpty() || password.isEmpty()){
+                    Toast.makeText(Signin.this, "No field can be left empty", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    timetostart();
+                }
+            }
+        });
 
     }
-    public void timetostart(View view){
+    public void timetostart(){
         Intent intent = new Intent(Signin.this,TimerClockActivity.class);
         startActivity(intent.cloneFilter());
     }
